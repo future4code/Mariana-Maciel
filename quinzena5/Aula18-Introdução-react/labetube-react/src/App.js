73b0eacf85fc5de2521c73ed16d999724552b54d
Mariana Maciel <marianamaciel@outlook.com>
2021-06-14 22:51:12 -0300

    exercicio-aula18

--- quinzena5/Aula18-Introdução-react/labetube-react/src/App.js
+++ quinzena5/Aula18-Introdução-react/labetube-react/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className="tela-inteira">
           <header>
               <h1>Lab Tube</h1>
-              <input type="text" placeholder="Busca" id="campoDeBusca" />
+              <input type="text" placeholder="Busca" id="campoDeBusca"/>
           </header>
 
           <main>
@@ -22,7 +22,7 @@ function App() {
                       <li className="botoes-meunu-vertical">Início</li>
                       <li className="botoes-meunu-vertical">Em alta</li>
                       <li className="botoes-meunu-vertical">Inscrições</li>
-                      <hr />
+                      <hr/>
                       <li className="botoes-meunu-vertical">Originais</li>
                       <li className="botoes-meunu-vertical">Histórico</li>
                   </ul>
@@ -30,35 +30,35 @@ function App() {
               
               <section className="painel-de-videos">
                   <div className="box-pagina-principal media1" onClick={reproduzVideo}>
-                      <img src="https://picsum.photos/400/400?a=1 " alt="" />
+                      <img src="https://picsum.photos/400/400?a=1 " alt=""/>
                       <h4>{titulo}</h4>
                   </div>
                   <div className="box-pagina-principal media2" onClick={reproduzVideo}>
-                      <img src="https://picsum.photos/400/400?a=2 " alt="" />
+                      <img src="https://picsum.photos/400/400?a=2 " alt=""/>
                       <h4>{titulo}</h4>
                   </div>
                   <div className="box-pagina-principal media3" onClick={reproduzVideo}>
-                      <img src="https://picsum.photos/400/400?a=3 " alt="" />
+                      <img src="https://picsum.photos/400/400?a=3 " alt=""/>
                       <h4>{titulo}</h4>
                   </div>
                   <div className="box-pagina-principal media4" onClick={reproduzVideo}>
-                      <img src="https://picsum.photos/400/400?a=4 " alt="" />
+                      <img src="https://picsum.photos/400/400?a=4 " alt=""/>
                       <h4>{titulo}</h4>
                   </div>
                   <div className="box-pagina-principal media5" onClick={reproduzVideo}>
-                      <img src="https://picsum.photos/400/400?a=5 " alt="" />
+                      <img src="https://picsum.photos/400/400?a=5 " alt=""/>
                       <h4>{titulo}</h4>
                   </div>
                   <div className="box-pagina-principal media6" onClick={reproduzVideo}>
-                      <img src="https://picsum.photos/400/400?a=6 " alt="" />
+                      <img src="https://picsum.photos/400/400?a=6 " alt=""/>
                       <h4>{titulo}</h4>
                   </div>
                   <div className="box-pagina-principal media7" onClick={reproduzVideo}>
-                      <img src="https://picsum.photos/400/400?a=7 " alt="" />
+                      <img src="https://picsum.photos/400/400?a=7 " alt=""/>
                       <h4>{titulo}</h4>
                   </div>
                   <div className="box-pagina-principal media8" onClick={reproduzVideo}>
-                      <img src="https://picsum.photos/400/400?a=8 " alt="" />
+                      <img src="https://picsum.photos/400/400?a=8 " alt=""/>
                       <h4>{titulo}</h4>
                   </div>
               </section>
